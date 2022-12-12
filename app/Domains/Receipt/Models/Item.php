363f1c2b318067cf24dfd9a359b2ecea3027e98b
Mohamed Ibrahim <mohamed.ibr1994@gmail.com>
2022-12-12 14:33:31 +0200

    add Inventory.php

--- app/Domains/Receipt/Models/Item.php
+++ app/Domains/Receipt/Models/Item.php
@@ -3,6 +3,7 @@
 namespace App\Domains\Receipt\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Item extends Model
 {
@@ -12,4 +13,9 @@ class Item extends Model
      * @var string
      */
     protected $table = 'rps.document_item';
+
+    public function inventory(): BelongsTo
+    {
+        return $this->belongsTo(Inventory::class, 'invn_sbs_item_sid', 'sid');
+    }
 }
